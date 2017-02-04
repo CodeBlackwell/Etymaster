e6f80c62e6df304fd8fc41e71b1f9a60695a72e1
LeChristopher Blackwell <codeblackwell@gmail.com>
2017-02-03 20:11:32 -0600

    wip

--- scripts/scrape_wiki.py
+++ scripts/scrape_wiki.py
@@ -15,12 +15,24 @@ for table in tables:
     headers = []
     data = []
     for row in table:
-
+        index = 1
         # check each row to see if it is a header row
-        # if hasattr(row, "th"):
-        #     headers.append([row.text])
-        # elif hasattr(row, "td"):
-        #     data.append(row)
-    listOfTables.append({"headers": headers, "data": data})
+        # if hasattr(row, "th") == True:
+        #     print({row: row, name: row.name, HoD: 'header'})
+        #     headers.append(row.string)
+        # if hasattr(row, "td") == True:
+        #     print({row: row, name: row.name, HoD:'data'})
+        #     data.append(row.string)
 
-print(listOfTables[0]['headers'][0])
+    listOfTables.append({ "data": data, "headers": headers })
+
+
+
+# {"citation_form": '', "declining_stem": '', "meaning": '', "english_derivatives": ''}
+print(listOfTables[0])
+
+
+print(type(tables))
+print(type(tables[0]))
+print(type(tables[0].tr))
+print(tables[0].tr.get("table"))
