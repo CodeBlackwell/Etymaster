90f412e8646d7a794f6b66754e5eb0cf5563bdc9
LeChristopher Blackwell <codeblackwell@gmail.com>
2017-02-05 23:34:45 -0600

    if num of elements == 4 imGood

--- scripts/scrape_wiki.py
+++ scripts/scrape_wiki.py
@@ -15,6 +15,18 @@ tables = keepers
 nav_string = type(tables[0].tr.next_sibling)
 tag = type(tables[0].tr)
 
+# print(tables)
+
+def validate_anchors(element):
+    result = []
+    if element.string == None:
+        anchors = bs.BeautifulSoup(str(element), 'lxml').find_all("a")
+        for anchor in anchors:
+            result.append(anchor.string)
+
+        return ", ".join(result)
+    else:
+        return element.string
 
 def is_empty(hash):
     empty = True
@@ -23,87 +35,99 @@ def is_empty(hash):
             empty = False
     return empty
 
-def get_prev_props(row, property):
-    if is_empty(row) == True:
-        get_prev_props(row)
+def get_prev_prop(list, index, property):
+    index = index - 1
+    return list[index][property]
 
+def num_of_elements(row):
+    return len(bs.BeautifulSoup(str(row), 'lxml').find_all("td"))
 
-nouns_and_adjectives = []
-nouns_and_adjectives_table_index = 0
+nouns_and_adj = []
+nouns_and_adj_index = 0
+
 for row in tables[0]:
-    row_index = 0
+    data_row_index = 0
     rowData = {"citation_form": None, "declining_stem": None, "meaning": None, "english_derivatives": None}
     # check each row to see if it is a tag object
+    # print(len(bs.BeautifulSoup(row, 'lxml')))
     for td in row:
         # check each <td> tag to ignore all headers and only pass tags
         # assign appropriate keys to all values
+        if num_of_elements(row) == 4:
+
         # @formatter:off
-        if type(td) == tag and td.name != 'th' and len(row) == 4:
-            if row_index == 0:
-                rowData["citation_form"]       = td.string
-                row_index = row_index + 1
+            if type(td) == tag and td.name != 'th':
 
-            elif row_index == 1:
-                rowData["declining_stem"]      = td.string
-                row_index = row_index + 1
+                if data_row_index == 0:
+                    rowData["citation_form"]       = validate_anchors(td)
+                    data_row_index = data_row_index + 1
 
-            elif row_index == 2:
-                rowData["meaning"]             = td.string
-                row_index = row_index + 1
+                elif data_row_index == 1:
+                    rowData["declining_stem"]      = validate_anchors(td)
+                    data_row_index = data_row_index + 1
 
-            elif row_index == 3:
-                rowData["english_derivatives"] = td.string
-                break
-    # @formatter:on
-        elif type(td) == tag and td.name != 'th' and len(row) == 2:
-            if row_index == 0:
-               rowData["citation_form"]       = td.string
-               row_index = row_index + 1
+                elif data_row_index == 2 :
+                    rowData["meaning"]             = validate_anchors(td)
+                    data_row_index = data_row_index + 1
 
-            elif row_index == 1:
-               rowData["declining_stem"]      = td.string
-               row_index = row_index + 1
+                elif data_row_index == 3:
+                    rowData["english_derivatives"] = validate_anchors(td)
+                    break
 
-            elif row_index == 2:
-               rowData["meaning"]             = table[]
-               row_index = row_index + 1
+        # elif num_of_elements(row) == 2:
 
-            elif row_index == 3:
-               rowData["english_derivatives"] = td.string
-               break
-       # @formatter:on
+            # if type(td) == tag and td.name != 'th':
+            #     if data_row_index == 0:
+            #         rowData["citation_form"]       = td.string
+            #         data_row_index = data_row_index + 1
+            #
+            #     elif data_row_index == 1:
+            #         rowData["declining_stem"]      = td.string
+            #         data_row_index = data_row_index + 1
+            #
+            #     elif data_row_index == 2:
+            #         rowData["meaning"]             = get_prev_prop(nouns_and_adj, nouns_and_adj_index, "meaning")
+            #         data_row_index = data_row_index + 1
+            #
+            #     elif data_row_index == 3:
+            #         rowData["english_derivatives"] = get_prev_prop(nouns_and_adj, nouns_and_adj_index, "english_derivatives")
+            #         break
+                    # @formatter:on
+        # else:
+        #     print "empty row"
+
     if is_empty(rowData) == False:
-        nouns_and_adjectives.append(rowData)
-    nouns_and_adjectives_table_index += 1
-# print(nouns_and_adjectives)
+        nouns_and_adj.append(rowData)
+        nouns_and_adj_index = nouns_and_adj_index + 1
+
+print(nouns_and_adj)
 
 verbs_table = []
 
 for row in tables[1]:
-    row_index = 0
+    data_row_index = 0
     rowData = {"citation_form": None, "declining_stem": None, "meaning": None, "english_derivatives": None}
     # check each row to see if it is a tag object
     for td in row:
         # assign appropriate keys to all values
         # @formatter:off
         if type(td) == tag and td.name != 'th':
-            if row_index == 0:
+            if data_row_index == 0:
                 rowData["citation_form"]       = td.text
-                row_index = row_index + 1
+                data_row_index = data_row_index + 1
 
-            elif row_index == 1:
+            elif data_row_index == 1:
                 rowData["declining_stem"]      = td.text
-                row_index = row_index + 1
+                data_row_index = data_row_index + 1
 
-            elif row_index == 2:
+            elif data_row_index == 2:
                 rowData["meaning"]             = td.text
-                row_index = row_index + 1
+                data_row_index = data_row_index + 1
 
-            elif row_index == 3:
+            elif data_row_index == 3:
                 rowData["english_derivatives"] = td.text
-                row_index = row_index + 1
+                data_row_index = data_row_index + 1
     #@formatter:on
-
     if is_empty(rowData) == False:
         verbs_table.append(rowData)
 # print(verbs_table)
