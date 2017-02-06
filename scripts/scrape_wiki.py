352f55d8f6e84501781a497ee06d17e37405cafd
LeChristopher Blackwell <codeblackwell@gmail.com>
2017-02-06 00:48:42 -0600

    wip

--- scripts/scrape_wiki.py
+++ scripts/scrape_wiki.py
@@ -15,18 +15,19 @@ tables = keepers
 nav_string = type(tables[0].tr.next_sibling)
 tag = type(tables[0].tr)
 
+
 # print(tables)
 
-def validate_anchors(element):
-    result = []
+def validate(element):
+    result = [element.text]
     if element.string == None:
         anchors = bs.BeautifulSoup(str(element), 'lxml').find_all("a")
         for anchor in anchors:
-            result.append(anchor.string)
-
-        return ", ".join(result)
+            # result.append(anchor.string)
+            return result
     else:
         return element.string
+
 
 def is_empty(hash):
     empty = True
@@ -35,12 +36,15 @@ def is_empty(hash):
             empty = False
     return empty
 
+
 def get_prev_prop(list, index, property):
     index = index - 1
     return list[index][property]
 
+
 def num_of_elements(row):
     return len(bs.BeautifulSoup(str(row), 'lxml').find_all("td"))
+
 
 nouns_and_adj = []
 nouns_and_adj_index = 0
@@ -48,62 +52,60 @@ nouns_and_adj_index = 0
 for row in tables[0]:
     data_row_index = 0
     rowData = {"citation_form": None, "declining_stem": None, "meaning": None, "english_derivatives": None}
-    # check each row to see if it is a tag object
-    # print(len(bs.BeautifulSoup(row, 'lxml')))
     for td in row:
-        # check each <td> tag to ignore all headers and only pass tags
-        # assign appropriate keys to all values
         if num_of_elements(row) == 4:
-
-        # @formatter:off
+            # @formatter:off
             if type(td) == tag and td.name != 'th':
-
+                valid = validate(td)
                 if data_row_index == 0:
-                    rowData["citation_form"]       = validate_anchors(td)
+                    rowData["citation_form"]       = valid
                     data_row_index = data_row_index + 1
 
                 elif data_row_index == 1:
-                    rowData["declining_stem"]      = validate_anchors(td)
+                    rowData["declining_stem"]      = valid
                     data_row_index = data_row_index + 1
 
                 elif data_row_index == 2 :
-                    rowData["meaning"]             = validate_anchors(td)
+                    rowData["meaning"]             = valid
                     data_row_index = data_row_index + 1
 
                 elif data_row_index == 3:
-                    rowData["english_derivatives"] = validate_anchors(td)
+                    rowData["english_derivatives"] = valid
                     break
 
-        # elif num_of_elements(row) == 2:
+        if num_of_elements(row) == 2:
 
-            # if type(td) == tag and td.name != 'th':
-            #     if data_row_index == 0:
-            #         rowData["citation_form"]       = td.string
-            #         data_row_index = data_row_index + 1
-            #
-            #     elif data_row_index == 1:
-            #         rowData["declining_stem"]      = td.string
-            #         data_row_index = data_row_index + 1
-            #
-            #     elif data_row_index == 2:
-            #         rowData["meaning"]             = get_prev_prop(nouns_and_adj, nouns_and_adj_index, "meaning")
-            #         data_row_index = data_row_index + 1
-            #
-            #     elif data_row_index == 3:
-            #         rowData["english_derivatives"] = get_prev_prop(nouns_and_adj, nouns_and_adj_index, "english_derivatives")
-            #         break
+            if type(td) == tag and td.name != 'th':
+                valid = validate(td)
+                if data_row_index == 0:
+                    rowData["citation_form"]       = valid
+                    data_row_index = data_row_index + 1
+
+                elif data_row_index == 1:
+                    rowData["declining_stem"]      = valid
+                    data_row_index = data_row_index + 1
+
+            if data_row_index == 2:
+                prev = nouns_and_adj_index - 1
+
+                # print({"index": nouns_and_adj_index, "meaning": valid})
+
+                rowData["meaning"]             = nouns_and_adj[prev]["meaning"]
+                data_row_index = data_row_index + 1
+
+            elif data_row_index == 3:
+                    rowData["english_derivatives"] = nouns_and_adj[prev]["english_derivatives"]
+                    break
                     # @formatter:on
-        # else:
-        #     print "empty row"
 
     if is_empty(rowData) == False:
         nouns_and_adj.append(rowData)
         nouns_and_adj_index = nouns_and_adj_index + 1
-
+# print (nouns_and_adj)
 print(nouns_and_adj)
 
 verbs_table = []
-
+# capitellum
 for row in tables[1]:
     data_row_index = 0
     rowData = {"citation_form": None, "declining_stem": None, "meaning": None, "english_derivatives": None}
